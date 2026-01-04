4f2a7436ce05677ec70853ad4c4bea04298174e6
xhemo1976 <webdjyoung@gmail.com>
2026-01-04 13:47:28 +0100

    mobile logo fix

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -13,7 +13,7 @@ export function HeroSection() {
                     src="/logo.png"
                     alt="Background"
                     fill
-                    className="object-cover object-center opacity-40 md:opacity-30 scale-95 md:scale-100"
+                    className="object-cover object-center opacity-40 md:opacity-25 scale100 md:scale-195 px-6 md:px-0"
                     priority
                 />
             </div>
